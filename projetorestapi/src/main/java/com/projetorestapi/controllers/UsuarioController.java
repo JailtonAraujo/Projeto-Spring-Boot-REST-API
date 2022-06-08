6836018da0ab9bebd91699d42d591c46a9d8f9c0
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2022-06-08 15:31:14 -0300

    update

--- projetorestapi/src/main/java/com/projetorestapi/controllers/UsuarioController.java
+++ projetorestapi/src/main/java/com/projetorestapi/controllers/UsuarioController.java
@@ -1,27 +1,57 @@
 package com.projetorestapi.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetorestapi.model.Usuario;
+import com.projetorestapi.repository.UsuarioRepository;
 
 @RequestMapping("/usuario")
 @RestController
 public class UsuarioController {
+	
+	@Autowired
+	UsuarioRepository usuarioRepository;
 
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity<Usuario> init() {
+	public ResponseEntity<List<Usuario>> BuscarTodos() {
 		
-		Usuario usuario = new Usuario();
-		usuario.setId(1L);
-		usuario.setNome("jailton");
-		usuario.setLogin("jailton10");
-		usuario.setSenha("1234");
+		return new ResponseEntity<List<Usuario>>(usuarioRepository.findAll(), HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/{nome}", produces = "application/json")
+	public ResponseEntity<Usuario> BuscarNome (@PathVariable(name = "nome") String nome){
 		
-		
-		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+		return new ResponseEntity<Usuario>(usuarioRepository.findByName(nome), HttpStatus.OK);
+	}
+	
+	
+	@PostMapping(value="/", produces = "application/json")
+	public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){
+		usuarioRepository.save(usuario);
+		return new ResponseEntity<Usuario>(HttpStatus.OK);
+	}
+	
+	
+	@DeleteMapping(value = "/{id}", produces = "application/json")
+	public String deletar(@PathVariable(name = "id") Long id){
+		usuarioRepository.deleteById(id);
+		return  "OK";
+	}
+	
+	@PutMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<Usuario> atualizar(@PathVariable(name = "id") Long id){
+		return new ResponseEntity<Usuario>(usuarioRepository.findById(id).get(), HttpStatus.OK);
 	}
 }
---
+++ projetorestapi/src/main/java/com/projetorestapi/repository/UsuarioRepository.java
@@ -0,0 +1,17 @@
+package com.projetorestapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.projetorestapi.model.Usuario;
+
+@Transactional
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	
+	@Query("select u from Usuario u where u.nome = ?1")
+	public Usuario findByName(String nome);
+
+}
